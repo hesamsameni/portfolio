ab31d4cbad303cf42bd8efbdd432a1cd19839fb0
Hesam Sameni <hesam.sameni@gmail.com>
2025-01-11 14:54:44 +0000

    Adding theme selector

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="container mx-auto max-w-7xl">
-    <header class="flex justify-between items-center mt-5">
-      <div>
-        <nuxt-link to="/" class="text-xl font-semibold p-5 hover:text-gray-500">
-          Hesam Sameni
-        </nuxt-link>
+    <header class="flex justify-between items-start md:items-center mt-5">
+      <div class="flex items-center md:space-x-12">
+        <div class="hidden md:block">
+          <NuxtLink
+            to="/"
+            class="text-xl font-semibold p-2 hover:bg-gray-200 dark:hover:bg-gray-800"
+            >Hesam Sameni
+          </NuxtLink>
+        </div>
+
+        <Menu />
       </div>
-      <Menu />
+
+      <ClientOnly>
+        <ColorModeSelector />
+      </ClientOnly>
     </header>
     <main class="p-5">
       <slot />
